3cf9cd534cf5d36ccc50cc7f34bff1df2f16032b
MarahAyman <marah2004memo@gmail.com>
2026-02-08 21:07:21 +0300

    Refactor: Integrated accounts and skills with modular routing

--- core/urls.py
+++ core/urls.py
@@ -1,14 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from skills.views import SkillViewSet, RegisterView # أضفنا RegisterView هنا
-
-# إعداد الراوتر اللي رح ينشئ الروابط تلقائياً
-router = DefaultRouter()
-router.register(r'skills', SkillViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(router.urls)), # هاد هو الرابط اللي رح تحتاجه بسومة
-    path('api/register/', RegisterView.as_view(), name='register'),
+    path('api/accounts/', include('accounts.urls')), 
+    path('api/', include('skills.urls')), 
 ]
--- skills/serializers.py
+++ skills/serializers.py
@@ -1,24 +1,7 @@
-from django.contrib.auth.models import User
 from rest_framework import serializers
-from .models import Skill # استدعاء الموديل اللي برمجتيه سوا
+from .models import Skill # استدعاء الموديل اللي برمجتيه سةا ص
 
 class SkillSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Skill
-        fields = '__all__' # هاد السطر بيحكي للدجانغو: "حول كل أعمدة الجدول لنصوص
-        
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ['username', 'email', 'password']
-        extra_kwargs = {'password': {'write_only': True}} # عشان الباسورد ما يرجع يظهر في الـ API للأمان
-
-    def create(self, validated_data):
-        # تشفير كلمة المرور قبل الحفظ في قاعدة البيانات
-        user = User.objects.create_user(**validated_data)
-        return user
-    class SkillSerializer(serializers.ModelSerializer):
      owner = serializers.ReadOnlyField(source='owner.username') # يقرأ اسم المستخدم فقط
 
      class Meta:
